8ae71d92b312d524c58f3a32dc3bf4e516efcf03
Litos <windsochi@gmail.com>
2014-07-21 20:54:27 +0400

    Simple maket for app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlashcardStatic::Application.config.secret_key_base = 'dcc5ecb33ef5b5bd6db36ff2c0e09a2ea7a589c27690b00942d089a62c3bfbd0b8e487cf75e558f0250dabcb75cba705477fb8e389d9cf8fd853be1867b4678c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlashcardStatic::Application.config.session_store :cookie_store, key: '_flashcard_static_session'
