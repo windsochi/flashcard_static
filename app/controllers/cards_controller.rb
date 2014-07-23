b947201d29e885b6c8c078f0acd81dcbc85cb3d6
Litos <windsochi@gmail.com>
2014-07-23 16:45:12 +0400

    Change cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,7 +2,17 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+  
   def edit
     @card = Card.find(params[:id])
+  end
+  
+  def update
+    @card = Card.find(params[:id])
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      render 'edit'
+    end
   end
 end
