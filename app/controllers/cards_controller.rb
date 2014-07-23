77d516c31cc5374385c8e8fcde8144e59863f0bd
Litos <windsochi@gmail.com>
2014-07-23 17:43:26 +0400

    add metod show & show.html.erb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,6 +5,16 @@ class CardsController < ApplicationController
   
   def new
   end
+
+  def create
+    @card = Card.new(card_params)
+    @card.save
+    redirect_to cards_path
+  end
+  
+  def show
+    @card = Card.find(params[:id])
+  end
   
   def edit
     @card = Card.find(params[:id])
