b2fd9627a1fcaa61e72c88803bb05bf97708c5f2
Litos <windsochi@gmail.com>
2014-07-27 00:13:09 +0400

    add code View & Controller

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,14 @@
 class IndexController < ApplicationController
   def index
+    @card = Card.where("review_date <= ?", Time.now).limit(1)
+
+    otvet = params[:search]
+  
+    if otvet  == 1
+      flash[:notice] = "Verno"
+    else
+      flash[:notice] = "Neverno"
+    end
+
   end
 end
