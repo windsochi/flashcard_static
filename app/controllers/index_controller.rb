5d3436cefa18ba1a7b468416fa529c5f028fc27c
Litos <windsochi@gmail.com>
2014-07-27 19:35:15 +0400

    Fix bugs

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,14 +1,9 @@
 class IndexController < ApplicationController
   def index
-    @card = Card.where("review_date <= ?", Time.now).limit(1)
-
-    otvet = params[:search]
-  
-    if otvet  == 1
+  if params[:search] == Card.first_card[:original_text] 
       flash[:notice] = "Verno"
-    else
-      flash[:notice] = "Neverno"
+      else
+        flash[:notice] = "Neverno"
     end
-
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,4 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, presence: true
+  scope :first_card, -> { where("review_date <= ?", Time.now).first }
 end
