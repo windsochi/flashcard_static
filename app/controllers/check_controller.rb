8b0a5968e772fe8f40f08b82ab85043be8364aaf
Litos <windsochi@gmail.com>
2014-07-28 18:34:23 +0400

    check_controller.rb & fix bug

--- app/controllers/check_controller.rb
+++ app/controllers/check_controller.rb
@@ -1,12 +1,13 @@
 class CheckController < ApplicationController
   def index
-    random_card = Card.due.first
-    if params[:search] == random_card.check(params[:search])
-      flash[:notice] = "Correct"
+  @random_card = Card.due.first
+    if params[:search] == @random_card.translated_text
+      @random_card.review_date + 2.days
+      @random_card.save
       redirect_to root_path
+      flash[:notice] = "Correct ..."
     else
-      flash[:notice] = "Incorrect"
       redirect_to root_path
+      flash[:notice] = "Incorrect ..."
     end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FlashcardStatic::Application.routes.draw do
   root 'index#index'
   resources :cards
+  resources :check
 end
