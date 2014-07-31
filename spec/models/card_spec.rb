3fb01e8fd880eeb2722d8053de11eb0dfd9f80e9
Litos <windsochi@gmail.com>
2014-07-31 17:46:50 +0400

    rewrite card_spec.rb

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Card, :type => :model do
+
   before do
-    @card = Card.create!(original_text: "House", translated_text: "Дом", review_date: Time.now - 4.days)
+    @card = Card.create!(original_text: "House", translated_text: "Дом", review_date: Time.now - 3.days)
   end
 
   it "check translated_text & original_text" do
-	expect(@card.check_translation("Домик")).should be_false
+	expect(@card.check_translation("Домик")).to be false
   end
 
   it "Update review_date = + 3.days" do
-    @card.create_card.should be_true
+    expect(@card.create_card) == (Time.now + 3.days)
   end
 end
