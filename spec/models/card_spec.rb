964cd8604b77266561b16f41834eb7460d51c400
Litos <windsochi@gmail.com>
2014-07-31 23:15:30 +0400

    change card_spec.rb

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Card, :type => :model do
+RSpec.describe Card do
 
   before do
     @card = Card.create!(original_text: "House", translated_text: "Дом", review_date: Time.now - 3.days)
   end
 
-  it "check translated false" do
+  it "check translated for false" do
     expect(@card.check_translation("Домик")).to be false
   end
 
-  it "check translated true" do
+  it "check translated for true" do
     expect(@card.check_translation("Дом")) == (@card.translated_text)
   end
 
-  it "Update review_date = + 3.days" do
+  it "Updates reviews date" do
     expect(@card.create_card) == (Time.now + 3.days)
   end
 
