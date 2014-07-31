dc83281c6cfd7dedacb568a0578c01f6194264f5
Litos <windsochi@gmail.com>
2014-07-31 17:52:12 +0400

    + fix clear code

--- Gemfile
+++ Gemfile
@@ -9,12 +9,13 @@ gem 'jbuilder', '~> 1.2'
 gem 'pg'
 gem 'rails_12factor', '0.0.2'
 gem 'simple_form'
+
 group :development, :test do
   gem 'better_errors'
   gem 'debugger'
   gem 'rspec-rails', '~> 3.0.0'
 end
+
 group :doc do
-  # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Card, :type => :model do
-
+  
   before do
     @card = Card.create!(original_text: "House", translated_text: "Дом", review_date: Time.now - 3.days)
   end
 
   it "check translated_text & original_text" do
-	expect(@card.check_translation("Домик")).to be false
+  	expect(@card.check_translation("Домик")).to be false
   end
 
   it "Update review_date = + 3.days" do
     expect(@card.create_card) == (Time.now + 3.days)
   end
+  
 end
