db74b2f71dc700aee2e57421ce96854973ca4b34
Litos <windsochi@gmail.com>
2014-07-31 19:24:05 +0400

    fix blank spaces

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Card, :type => :model do
-  
+
   before do
     @card = Card.create!(original_text: "House", translated_text: "Дом", review_date: Time.now - 3.days)
   end
 
   it "check translated false" do
-  	expect(@card.check_translation("Домик")).to be false
+    expect(@card.check_translation("Домик")).to be false
   end
 
   it "check translated true" do
@@ -17,5 +17,5 @@ RSpec.describe Card, :type => :model do
   it "Update review_date = + 3.days" do
     expect(@card.create_card) == (Time.now + 3.days)
   end
-  
+
 end
