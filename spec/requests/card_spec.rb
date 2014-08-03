967697e8ddd65912ce5bfcf11a5996288ae5dabf
Litos <windsochi@gmail.com>
2014-08-03 20:25:17 +0400

    fix bugs & capybara include

--- spec/requests/card_spec.rb
+++ spec/requests/card_spec.rb
@@ -8,7 +8,10 @@ RSpec.describe "Managing cards" do
 
   it "should be able to add card" do
   	expect(page).to have_title 'Флэшкарточкер | Новая карточка'
-
+  	fill_in 'Review date', :with => Time.now - 4.days
+  	fill_in 'Original text', :with => "Qwerty"
+  	fill_in 'Translated text', :with => "Йцукен"
+  	click_button('Save Card')
+    expect(page).to have_title 'Флэшкарточкер | Все карточки'
   end
-
 end
