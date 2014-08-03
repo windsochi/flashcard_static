01d3356b0d9947163229f3c428df40c118cf3f9f
Litos <windsochi@gmail.com>
2014-08-03 22:39:20 +0400

    fix bug factory-girl spec & capybara

--- spec/requests/card_spec.rb
+++ spec/requests/card_spec.rb
@@ -4,13 +4,14 @@ RSpec.describe "Managing cards" do
 
   before do
     visit new_card_path
+    @card = FactoryGirl.create(:card)
   end
 
   it "should be able to add card" do
   	expect(page).to have_title 'Флэшкарточкер | Новая карточка'
-  	fill_in 'Review date', :with => Time.now - 4.days
-  	fill_in 'Original text', :with => "Qwerty"
-  	fill_in 'Translated text', :with => "Йцукен"
+    fill_in 'Review date', :with => @card.review_date
+    fill_in 'Original text', :with => @card.original_text
+    fill_in 'Translated text', :with => @card.translated_text
   	click_button('Save Card')
     expect(page).to have_title 'Флэшкарточкер | Все карточки'
   end
