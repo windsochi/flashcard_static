ef819a84da5aeb394e2ea4c65e70400a1fe9b26b
Litos <windsochi@gmail.com>
2014-08-04 20:44:43 +0400

    add expect for card_spec.rb, 17 line

--- spec/requests/card_spec.rb
+++ spec/requests/card_spec.rb
@@ -14,21 +14,23 @@ RSpec.describe "Managing cards" do
     fill_in 'Original text', with: card.original_text
     fill_in 'Translated text', with: card.translated_text
     click_button('Save Card')
+    expect(Card.last.check_translation("Вертолётик")).to be false
+    expect(Card.last.check_translation("Вертолет")) == (Card.last.translated_text)
   end
 
-  it "should be able card for root_path" do
+  it "should show card on root page" do
     visit cards_path
     expect(page).to have_content 'Helicopter'
   end
 
-  it "should be able check translated for correct" do
+  it "should show correct message for correct translation" do
     visit root_path
     fill_in 'search', with: 'Вертолет'
     click_button('Verify')
     expect(page).to have_content 'Correct!'
   end
 
-  it "should be able check translated for incorrect" do
+  it "should show correct message for incorrect translation" do
     visit root_path
     fill_in 'search', with: 'Вертолётик'
     click_button('Verify')
