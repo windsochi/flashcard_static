a587608f1aa4756435067c88127a69194ef6b880
Litos <windsochi@gmail.com>
2014-08-04 19:46:51 +0400

    fix bug card_spec.rb

--- spec/requests/card_spec.rb
+++ spec/requests/card_spec.rb
@@ -5,12 +5,11 @@ RSpec.describe "Managing cards" do
   let!(:card) { FactoryGirl.create(:card) }
 
   before do
-    visit new_card_path
     FactoryGirl.create(:card)
   end
 
   it "should be able to add card" do
-    expect(page).to have_title 'Флэшкарточкер | Новая карточка'
+    visit new_card_path
     fill_in 'Review date', with: card.review_date
     fill_in 'Original text', with: card.original_text
     fill_in 'Translated text', with: card.translated_text
