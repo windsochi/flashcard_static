6c4091ea4a07ea1bbde286d683c7313d03bc36fd
Litos <windsochi@gmail.com>
2014-08-11 00:59:56 +0400

    add_columns db:migrate

--- db/migrate/20140810191817_create_users.rb
+++ db/migrate/20140810191817_create_users.rb
@@ -5,10 +5,5 @@ class CreateUsers < ActiveRecord::Migration
       t.text :password
       t.timestamps
     end
-
-    create_table :cards do |t|
-      t.belongs_to :user, index: true
-      t.timestamps
-    end
   end
 end
