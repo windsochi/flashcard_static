291d3f23aaf3027e7b3b97877e1a794d20c212d1
Litos <windsochi@gmail.com>
2014-08-13 12:10:36 +0400

    application.html.erb - add code for sorcery

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :cards
   authenticates_with_sorcery!
 
-  validates :password, length: {minimum: 3}
+  validates :password, length: { minimum: 3 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
