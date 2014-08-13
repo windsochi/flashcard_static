d764f5e0cac1daca5fa8bf5bfdf9af2e7fb34710
Litos <windsochi@gmail.com>
2014-08-13 12:13:55 +0400

    routes.rb - add routes for Sorcery & Users RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 FlashcardStatic::Application.routes.draw do
   root 'index#index'
   resources :cards
+  resources :users
   post 'reviews', to: 'reviews#create'
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  Sorcery::Application.routes.draw do
+    get "user_sessions/new"
+    get "user_sessions/create"
+    get "user_sessions/destroy"
+  end
+
 end
