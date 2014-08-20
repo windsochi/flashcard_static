88044343b63b3d75c276953e37e54d8ed2541bbb
Litos <windsochi@gmail.com>
2014-08-20 11:13:02 +0400

    fix

--- spec/requests/card_manager_spec.rb
+++ spec/requests/card_manager_spec.rb
@@ -2,21 +2,20 @@ require 'rails_helper'
 
 describe "Managing cards" do
 
-  let!(:user) { create(:user) }
+  let!(:user) { create(:user, email: "windsochi@gmail.com", password: "12345") }
   let!(:card) { create(:card, user_id: user.id) }
 
   it "should be able to add card" do
     visit login_path
     fill_in 'email', with: "windsochi@gmail.com"
-    fill_in 'password', with: '12345'
+    fill_in 'password', with: "12345"
     click_button('Login')
 
     visit new_card_path
     fill_in 'Review date', with: card.review_date
     fill_in 'Original text', with: card.original_text
     fill_in 'Translated text', with: card.translated_text
-    find(:xpath, "//input[@id='card_user_id']").set "1"
-    click_button('Создать')
+    click_button('Save Card')
     expect(page).to have_content 'Карточка успешно создана'
   end
 
