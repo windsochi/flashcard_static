9fc3151d33cb44e60f9738aa8c69cfe8bd7ac1e8
Litos <windsochi@gmail.com>
2014-08-25 23:14:26 +0400

    added resize_t_fit

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,8 +1,8 @@
 class PictureUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
-
   storage :file
+  process :resize_to_fit => [360, 360]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
