60c3e085607d44d78923b2818dfd56ad2ba5d5d8
Litos <windsochi@gmail.com>
2014-08-27 17:32:56 +0400

    fix .gitignore - remove carriervawe.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
   config.fog_directory  = 'fcwindsochi'
 
   config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAJOPEGYOBDBXDUGQQ',
-    :aws_secret_access_key  => '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',
-    :region                 => 'us-east-1',
+    provider: 'AWS',
+    aws_access_key_id 'AKIAJOPEGYOBDBXDUGQQ',
+    aws_secret_access_key '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',
+    region 'eu-west-1',
   }
 end
