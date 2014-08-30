aa26784ebffa9155cd3b33636e17bc478906220f
Litos <windsochi@gmail.com>
2014-08-30 12:32:02 +0400

    replaced settings for FOG -> hash

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
 
-  config.fog_directory  = Settings.aws.direct
+  config.fog_directory  = Settings[:aws][:direct]
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Settings.aws.akey,
-    aws_secret_access_key: Settings.aws.sakey,
+    aws_access_key_id: Settings[:aws][:akey],
+    aws_secret_access_key: Settings[:aws][:sakey],
     region: 'eu-west-1'
   }
 end
