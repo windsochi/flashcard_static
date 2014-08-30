c2f0b032ed72ae648f853917c0da8cc7bcbb731a
Litos <windsochi@gmail.com>
2014-08-30 12:41:45 +0400

    Yet another replaced settings rails_config 2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
 
-  config.fog_directory  = Settings.aws.direct
+  config.fog_directory  = 'fcwindsochieu'
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Settings.aws.akey,
-    aws_secret_access_key: Settings.aws.sakey,
+    aws_access_key_id: 'AKIAINN543DYUSEROQGQ',
+    aws_secret_access_key: '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',
     region: 'eu-west-1'
   }
 end
