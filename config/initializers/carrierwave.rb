2aef71057a821a1b6a1d062c4cabc185052b41a1
Litos <windsochi@gmail.com>
2014-08-30 23:38:35 +0400

    Uppercase var config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
 
-  config.fog_directory = AppSettings.s3_direct
+  config.fog_directory = AppSettings.S3_DIRECT
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: AppSettings.s3_key,
-    aws_secret_access_key: AppSettings.s3_secret,
+    aws_access_key_id: AppSettings.S3_KEY,
+    aws_secret_access_key: AppSettings.S3_SECRET,
     region: 'eu-west-1'
   }
 end
