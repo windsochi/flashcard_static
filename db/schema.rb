107cb4805746bc31d0e3ab8a772a8c578c2fee98
Litos <windsochi@gmail.com>
2014-08-30 19:16:44 +0400

    3 try for fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,32 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 20140825150237) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cards", force: true do |t|
+    t.integer  "user_id"
     t.text     "original_text"
     t.text     "translated_text"
     t.datetime "review_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
     t.string   "picture"
   end
-  add_index "cards", ["user_id"]
+
+  add_index "cards", ["user_id"], name: "index_cards_on_user_id", unique: true, using: :btree
 
   create_table "users", force: true do |t|
     t.text     "email"
@@ -20,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140825150237) do
     t.datetime "updated_at"
     t.string   "salt"
   end
+
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
