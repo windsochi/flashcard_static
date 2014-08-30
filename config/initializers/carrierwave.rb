9e306438c505bcbb1be139a2d2921acc11d563ec
Litos <windsochi@gmail.com>
2014-08-30 20:24:38 +0400

    3 try replaced settings for AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
 
-  config.fog_directory = ENV['Settings.aws.direct']
+  config.fog_directory = ENV['S3_DIRECT']
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['Settings.aws.akey'],
-    aws_secret_access_key: ENV['Settings.aws.sakey'],
+    aws_access_key_id: ENV['S3_KEY'],
+    aws_secret_access_key: ENV['S3_SECRET'],
     region: 'eu-west-1'
   }
 end
