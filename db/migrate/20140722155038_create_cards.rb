27b2e95ce2be85e5edddd208f1dde6a9d94d542c
Litos <windsochi@gmail.com>
2014-08-30 17:13:36 +0400

    fix bugs for migration

--- db/migrate/20140722155038_create_cards.rb
+++ db/migrate/20140722155038_create_cards.rb
@@ -3,9 +3,8 @@ class CreateCards < ActiveRecord::Migration
     create_table :cards do |t|
       t.text :original_text
       t.text :translated_text
-      t.datetime :review_dat
+      t.datetime :review_date
       t.timestamps
     end
-    add_index :cards, :user_id
   end
 end
--- db/migrate/20140810191817_create_users.rb
+++ db/migrate/20140810191817_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.text :email
-      t.text :password
+      t.text :crypted_password
       t.timestamps
     end
   end
