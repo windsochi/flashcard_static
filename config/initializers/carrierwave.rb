909be29a54967a26dc1098dfe143407de58410e1
Litos <windsochi@gmail.com>
2014-08-31 15:42:59 +0400

    replaced uppercase for down case, fix keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
 
-  config.fog_directory = Settings.S3_DIRECT
+  config.fog_directory = Settings.s3_direct
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Settings.S3_KEY,
-    aws_secret_access_key: Settings.S3_SECRET,
-    region: Settings.S3_REGION
+    aws_access_key_id: Settings.s3_key,
+    aws_secret_access_key: Settings.s3_secret,
+    region: Settings.s3_region
   }
 end
