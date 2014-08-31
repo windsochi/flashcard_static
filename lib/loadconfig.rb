0be6635a3d503a227f141e6376b0ebd65c649076
Litos <windsochi@gmail.com>
2014-08-31 18:18:37 +0400

    added ERB for loadconfig.rb

--- lib/loadconfig.rb
+++ lib/loadconfig.rb
@@ -3,6 +3,6 @@ module LoadConfigYml
   CONFIG_PATH = File.join(Rails.root, 'config/settings.yml')
 
   def self.config
-    @_config ||= YAML.load_file(CONFIG_PATH)[Rails.env]
+    @_config ||= YAML.load(ERB.new(File.read(CONFIG_PATH)).result)[Rails.env]
   end
 end
