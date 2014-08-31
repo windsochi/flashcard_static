daebfc0734a1b4811cbd6efc8e7bc4c2bb62a2aa
Litos <windsochi@gmail.com>
2014-08-31 14:54:10 +0400

    come back gem rails_config! Configured gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
 
-  config.fog_directory = ENV['S3_DIRECT']
+  config.fog_directory = Settings.S3_DIRECT
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['S3_KEY'],
-    aws_secret_access_key: ENV['S3_SECRET'],
-    region: 'eu-west-1'
+    aws_access_key_id: Settings.S3_KEY,
+    aws_secret_access_key: Settings.S3_SECRET,
+    region: Settings.S3_REGION
   }
 end
