d99f48a9c4b2f2ca99c66f7b08de05efd1dc86d6
Litos <windsochi@gmail.com>
2014-09-02 00:14:50 +0400

    added authorized for twitter

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -3,11 +3,16 @@ Rails.application.config.sorcery.submodules = [:external]
 
 Rails.application.config.sorcery.configure do |config|
   config.user_class = "User"
-  config.external_providers = [:github]
-  config.github.key =          LoadConfigYml.config['github_key']
-  config.github.secret =       LoadConfigYml.config['github_secret']
-  config.github.callback_url = LoadConfigYml.config['github_callback']
+  config.external_providers = [:github, :twitter]
+  config.github.key =           LoadConfigYml.config['github_key']
+  config.github.secret =        LoadConfigYml.config['github_secret']
+  config.github.callback_url =  LoadConfigYml.config['github_callback']
   config.github.user_info_mapping = {:email => "name"}
+
+  config.twitter.key =          LoadConfigYml.config['twitter_key']
+  config.twitter.secret =       LoadConfigYml.config['twitter_secret']
+  config.twitter.callback_url = LoadConfigYml.config['twitter_callback']
+  config.twitter.user_info_mapping = {:username => "screen_name"}
 
   config.user_config do |user|
     user.authentications_class = Authentication
