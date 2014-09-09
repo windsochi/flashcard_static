0989b1f51f911fd37867d6c664041079259add1f
Litos <windsochi@gmail.com>
2014-09-09 11:41:18 +0400

    added metod Correct, Incorrect answers.

--- app/models/card.rb
+++ app/models/card.rb
@@ -14,8 +14,26 @@ class Card < ActiveRecord::Base
     update_attributes(review_date: Time.now + 3.days)
   end
 
+  def correct_answer
+    increment(:number_correct_answers)
+    update_attribute(:number_incorrect_answers, 0)
+    update_review_date
+  end
+
+  def incorrect_answer
+    if number_incorrect_answers > 3
+      increment(:number_incorrect_answers)
+      save
+    else
+      decrement(:number_correct_answers) if number_correct_answers > 0
+      update_attribute(:number_incorrect_answers, 0)
+      update_review_date
+    end
+  end
+
+  end
   def set_review_date
-    self.review_date = Time.now - 1.days
+    self.review_date = Time.now
   end
 
 end
