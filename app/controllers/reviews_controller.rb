b986ec13d97d695412f4095d1cde4a90bba18b37
Litos <windsochi@gmail.com>
2014-09-15 16:58:55 +0400

    Third intermediate example service object

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def create
     card = current_user.cards.find(params[:card_id])
-    @result = ResultService.new(card.check_translation(params[:search]))
+    ResultService.new(card.check_translation(params[:search]))
     redirect_to root_path
   end
 
--- app/services/result_service.rb
+++ app/services/result_service.rb
@@ -1,16 +1,16 @@
 class ResultService
   def initialize(number_of_errors)
     if number_of_errors == 0
-      puts "0 error"
+      flash[:success] = "0 error"
       card.update_review_date
     elsif number_of_errors == 1
-      puts "1 error"
+      flash[:error] = "1 error"
     elsif number_of_errors == 2
-      puts "2 errors"
+      flash[:error] = "2 errors"
     elsif number_of_errors == 3
-      puts "3 errors"
+      flash[:error] = "3 errors"
     else number_of_errors == 4
-      puts "4 or more of errors"
+      flash[:error] = "4 or more of errors"
     end
   end
 end
