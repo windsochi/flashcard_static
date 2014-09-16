d1e16dfa93240af0615e2d8a8eb650698d02eb66
Litos <windsochi@gmail.com>
2014-09-16 10:35:39 +0400

    added object var

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
 class ReviewsController < ApplicationController
 
   def create
-    card = current_user.cards.find(params[:card_id])
-    @result = card.check_translation(params[:search])
+    @card = current_user.cards.find(params[:card_id])
+    @result = @card.check_translation(params[:search])
     redirect_to root_path
   end
 
